038a5b57e349f56099c91ba866edaf8e2edde675
Tamar Christina <tamar@zhox.com>
2016-12-18 20:16:29 +0000

    Win32: alignment

--- include/alignment.h
+++ include/alignment.h
@@ -1,3 +1,3 @@
 #if __GLASGOW_HASKELL__ < 711
-#let alignment t = "%lu", (unsigned long)offsetof(struct {char x__; t (y__); }, y__)
+#define hsc_alignment(t ) hsc_printf ( "%lu", (unsigned long)offsetof(struct {char x__; t(y__); }, y__));
 #endif
