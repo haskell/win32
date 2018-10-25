6996083b0b6560157c554e542e309a3f93ef81e7
Zhou Fangyi <fangyi.zhou@yuriko.moe>
2018-10-25 18:23:35 +0100

    Fix some broken links (Trac #15733) (#116)
    Summary:
    For links in subpackages as well.
    https://phabricator.haskell.org/D5257
    
    Test Plan:
    Verify link manually

--- Graphics/Win32/GDI.hs
+++ Graphics/Win32/GDI.hs
@@ -19,7 +19,7 @@
 -- The above warning supression flag is a temporary kludge.
 -- While working on this module you are encouraged to remove it and fix
 -- any warnings in the module. See
---     http://hackage.haskell.org/trac/ghc/wiki/WorkingConventions#Warnings
+--     https://ghc.haskell.org/trac/ghc/wiki/Commentary/CodingStyle#Warnings
 -- for details
 
 module Graphics.Win32.GDI (
