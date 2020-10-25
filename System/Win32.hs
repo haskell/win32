5438e722fad561a9647b84cedb21f02774e2bdae
NCrashed <ncrashed@gmail.com>
2020-10-25 10:21:43 +0300

    Add System.Win32.Event to reexport in System.Win32

--- System/Win32.hs
+++ System/Win32.hs
@@ -19,6 +19,7 @@
 
 module System.Win32
         ( module System.Win32.DLL
+        , module System.Win32.Event
         , module System.Win32.File
         , module System.Win32.FileMapping
         , module System.Win32.Info
@@ -40,6 +41,7 @@ module System.Win32
         ) where
 
 import System.Win32.DLL
+import System.Win32.Event
 import System.Win32.File
 import System.Win32.FileMapping
 import System.Win32.Info
