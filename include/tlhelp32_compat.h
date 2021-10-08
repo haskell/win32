cd69b1944f1afa4f85bca314e91c56f00cf3f921
Shekychan <sheks@hrt.pw>
2021-10-07 21:14:32 -0400

    add all flags for CreateToolhelp32Snapshot

--- include/tlhelp32_compat.h
+++ include/tlhelp32_compat.h
@@ -6,10 +6,21 @@
  */
 #if __GLASGOW_HASKELL__ > 708
 #else
-// Some declarations from tlhelp32.h that we need in Win32
+// Declarations from tlhelp32.h that Win32 requires
 #include <windows.h>
 
+// CreateToolhelp32Snapshot Flags
+// https://docs.microsoft.com/en-us/windows/win32/api/tlhelp32/nf-tlhelp32-createtoolhelp32snapshot
+
+#define TH32CS_INHERIT      0x80000000
+
+#define TH32CS_SNAPHEAPLIST 0x00000001
+#define TH32CS_SNAPPROCESS  0x00000002
+#define TH32CS_SNAPTHREAD   0x00000004
+#define TH32CS_SNAPMODULE   0x00000008
 #define TH32CS_SNAPMODULE32 0x00000010
+
+#define TH32CS_SNAPALL (TH32CS_SNAPHEAPLIST|TH32CS_SNAPPROCESS|TH32CS_SNAPTHREAD|TH32CS_SNAPMODULE)
 
 #endif
 #endif /* TLHELP32_COMPAT_H */
